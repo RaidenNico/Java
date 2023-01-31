a17181d323ae47c6b369b2bc348ec43449cd0e7d
Nicolas <na796840@gmail.com>
2023-01-30 20:28:28 -0500

    first example

---
+++ Java - SpringToolSuite4/SpringToolSuite4/app-inicial-spring/src/main/java/pe/edu/idat/AppInicialSpringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInicialSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInicialSpringApplication.class, args);
+	}
+
+}
