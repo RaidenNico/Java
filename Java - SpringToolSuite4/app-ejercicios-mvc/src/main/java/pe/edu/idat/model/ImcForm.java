cd8a7f167c7908df3ad185866b9fa6311abf87e0
Nicolas <na796840@gmail.com>
2023-02-04 00:37:07 -0500

    ejercicios

---
+++ Java - SpringToolSuite4/app-ejercicios-mvc/src/main/java/pe/edu/idat/model/ImcForm.java
@@ -0,0 +1,25 @@
+package pe.edu.idat.model;
+
+public class ImcForm {
+
+	private Double talla;
+	private Double peso;
+	public Double getTalla() {
+		return talla;
+	}
+	public void setTalla(Double talla) {
+		this.talla = talla;
+	}
+	public Double getPeso() {
+		return peso;
+	}
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	public ImcForm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
